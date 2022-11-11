d0f96cb404ed35c539b2c1ee20fb72b4ebedd79c
@shrayimjonov <shrayimjonov@uzkassa.uz>
2022-11-11 11:04:35 +0500

    Initial commit

---
+++ src/main/java/uz/company/redditapp/RedditAppApplication.java
@@ -0,0 +1,13 @@
+package uz.company.redditapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditAppApplication.class, args);
+    }
+
+}
